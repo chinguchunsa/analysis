02de4b744efc1388b8afab40fc4c929f0edd6508
MiKyung Lee <vos.harmony@gmail.com>
2019-12-09 13:25:13 -0500

    updated mod_misc

--- mod_misc.f90
+++ mod_misc.f90
@@ -40,7 +40,7 @@ contains
     integer(8) :: itime, i, ii, a, aa, ia
     real(8)    :: norm, ground, singles_a(noa), singles_b(nob)
     real(8)    :: ground_save(ntimes), singles_a_save(noa,ntimes), singles_b_save(nob,ntimes), ci_ten(10,ntimes)
-    complex(8) :: psi_det(nstates), psi2
+    complex(8) :: psi_det(nstates) 
     !: for writing
     character(5) :: c0, ci
     character(1000) :: ofile, ofile10
@@ -55,16 +55,19 @@ contains
     
 
     write(100,'(A)', advance='no') " time(fs) norm2 "
+    write(110,'(A)', advance='no') 'time(fs) norm ground'
     write( c0, '(i0)' ) 0
     write(100,'(A)', advance='no') trim(c0)//' '
+    write(110,'(A)', advance='no') trim(c0)//' '
     do i=1, noa
        write( c0, '(i0)' ) -i
        write(100,'(A)', advance='no') trim(c0)//' '
     end do
-    write(110,'(A)', advance='no') 'time(fs) norm ground'
     do i=1, nob
        write( c0, '(i0)' ) i
        write(100,'(A)', advance='no') trim(c0)//' '
+    end do
+    do i=1, 10
        write(110,'(A)', advance='no') trim(c0)//' '
     end do
     write(100,'(A)') ''
@@ -72,9 +75,9 @@ contains
     
 
     !$OMP PARALLEL DEFAULT(NONE), &
-    !$OMP PRIVATE( itime, singles_a, singles_b, norm, i, psi_det, ground, ii, ia,  &
+    !$OMP PRIVATE( itime, singles_a, singles_b, norm, i, psi_det, ground, ii, ia), &
     !$OMP SHARED(  ntimes, norm_sq, nstuse, nstates, ci_vec, psi, hole1, time,     &
-    !$OMP          ground_save, single_a_save, single_b_save, ci_ten )
+    !$OMP          ground_save, singles_a_save, singles_b_save, ci_ten )
     !$OMP DO
     do itime=1, ntimes
 
@@ -84,12 +87,12 @@ contains
        call get_psi_det( i, nstuse, nstates, ci_vec, psi(:,itime), psi_det )
        psi_det = psi_det / dsqrt(norm)
        
-       ground = dconjg(psi_det(1)) * psi_det(1)       
+       ground = dconjg(psi_det(1)) * psi_det(1)
        
        do ia=2, 10
           ii = hole1(ia)
           i  = abs(ii)
-          ci_ten(ia,itime) = dconjg(psi_det(ia)) * psi_det(ia)
+          ci_ten(ia,itime) = dconjg(psi(ia,itime))*psi(ia,itime)/norm
           if ( ii.lt.0 ) singles_a(i) = singles_a(i) + dconjg(psi_det(ia)) * psi_det(ia)
           if ( ii.gt.0 ) singles_b(i) = singles_b(i) + dconjg(psi_det(ia)) * psi_det(ia)
        end do
@@ -108,7 +111,8 @@ contains
     !$OMP END DO
     !$OMP END PARALLEL
        
-
+    
+    !: write 
     do itime=1, ntimes
        write(200,"(f10.7)",advance='no') time(itime), norm_sq(itime), ground_save(itime)
        write(200,"(f10.7)",advance='no' ) ( singles_a_save(i,itime) , i=1, noa )
@@ -116,14 +120,19 @@ contains
        write(200,'(A)') ''
     end do
 
+    !: write
     do itime=1, ntimes 
        write(110,'(3(1x,f15.10))', advance='no') time(itime), norm, ground    
-       write(110,'(1x,f15.10)', advance='no' ) ( psi_ten(ia,itimes), ia=1, 10 )
+       write(110,'(1x,f15.10)', advance='no' ) ( ci_ten(ia,itime), ia=1, 10 )
        write(110,'(A)') ''
     end do
     
     close(110)
     close(200)
+    
+    write(iout,'(A)') ' --> generated file '//trim(ofile)
+    write(iout,'(A)') ' --> generated file '//trim(ofile10)
+    flush(iout)
     
 
   end subroutine summarize_coefficients
@@ -136,24 +145,30 @@ contains
 
     integer(8), intent(in) :: idir
 
-    integer(8) :: itime, i, x, j, a, b, ia, ii, xx, jj, aa, bb
-    real(8)    :: singles(nob), doubles_ab(noa,nob), doubles_bb(nob,nob), norm
-    complex(8) :: psi_det(nstates), psi2
+    integer(8) :: itime, i, x, a, b, ia, ii, xx, aa
+    real(8) :: singles(nob), doubles_ab(noa,nob), doubles_bb(nob,nob), norm
+    real(8) :: singles_save(nob,ntimes), doubles_ab_save(noa,nob,ntimes), doubles_bb_save(nob,nob,ntimes), ci10_save(10,ntimes)
+    complex(8) :: psi_det(nstates)
     character(5) :: c0, cx, ci
-    character(1000) :: ofile, pyfile
+    character(1000) :: ofile, ofile10
 
+    
+    !: only set up for single BETA excitation
 
+    
     write( c0, '(i0)' ) idir
-    ofile  = 'SUMMARY_CI'//trim(e_d)//trim(c0)//'.out'
-    pyfile = 'SUMMARY_CI_PY'//trim(e_d)//trim(c0)//'.out' 
+    ofile   = 'SUMMARY_CI'//trim(e_d)//trim(c0)//'.out'
+    ofile10 = 'SUMMARY_CI10'//trim(e_d)//trim(c0)//'.out'
 
-    open( unit=100, file=trim(ofile) )
-    open( unit=200, file=trim(pyfile) )
-
+    open( unit=110, file=trim(ofile10) )
+    open( unit=200, file=trim(ofile) )
+    
     write(200,'(A)', advance='no') " time(fs) norm2 "
+    write(110,'(A)', advance='no') " time(fs) norm2 "
     do x=1, nob
        write( c0, '(i0)' ) x 
        write(200,'(A)', advance='no') trim(c0)//' '
+       write(110,'(A)', advance='no') trim(c0)//' '
     end do
     do x=1, nob
        write( c0, '(i0)' ) x
@@ -170,8 +185,14 @@ contains
        end do
     end do
     write(200,'(A)') ''
+    write(110,'(A)') ''
+
     
-    
+    !$OMP PARALLEL DEFAULT(NONE), &
+    !$OMP PRIVATE( itime, i, singles, doubles_ab, doubles_bb, psi_det, norm, xx, ii, aa ), &
+    !$OMP SHARED(  ntimes, nob, psi, ci_vec, nstuse, nstates, norm_sq, hole, part, singles_save,&
+    !$OMP          doubles_ab_save, doubles_bb_save, ci10_save )
+    !$OMP DO    
     do itime=1, ntimes
        
        singles    = 0.d0
@@ -181,126 +202,60 @@ contains
        norm = norm_sq(itime)
        call get_psi_det( i, nstuse, nstates, ci_vec, psi(:,itime), psi_det )
        psi_det = psi_det / dsqrt(norm)   
-
-       do ia=1, nstates
-
+       
+       do ia=1, nob          
           xx = hole(ia,1)
           ii = hole(ia,2)
           aa = part(ia,1)
+          if ( aa .eq. 0 ) singles(xx) = singles(xx) + dconjg(psi_det(ia)) * psi_det(ia) 
+          ci10_save(ia,itime) = dconjg( psi(ia,itime) ) * psi(ia,itime) / norm          
+       end do
 
-          psi2 = dconjg(psi_det(ia)) * psi_det(ia)
-
+       do ia=nob+1, nstates          
+          xx = hole(ia,1)
+          ii = hole(ia,2)
+          aa = part(ia,1)
           if ( aa .eq. 0 ) then
-             singles(xx) = singles(xx) + psi2
+             singles(xx) = singles(xx) + dconjg(psi_det(ia)) * psi_det(ia)
           else
-             if ( ii.lt.0 ) then
-                doubles_ab(-ii,xx) = doubles_ab(-ii,xx) + psi2
-             else
-                doubles_bb(ii,xx) = doubles_bb(ii,xx) + psi2
-                doubles_bb(xx,ii) = doubles_bb(xx,ii) + psi2
-             end if
+             if ( ii.lt.0 ) doubles_ab(-ii,xx) = doubles_ab(-ii,xx) + dconjg(psi_det(ia)) * psi_det(ia)
+             if ( ii.gt.0 ) doubles_bb(ii,xx)  = doubles_bb(ii,xx) + dconjg(psi_det(ia)) * psi_det(ia)
           end if
-          
        end do
 
-       write(100,"(' time(fs) ',f7.3, ' norm2 ', f15.10 )") time(itime), norm
-       write(100,41) ( singles(i), i=1, nob )
-       do x=1, nob
-          write(100,42) x, ( doubles_ab(i,x), i=1, noa )
-       end do
-       do x=1, nob
-          write(100,43) x, ( doubles_bb(i,x), i=1, nob ) 
-       end do
+       singles_save(:,itime) = singles(:)
+       doubles_ab_save(:,:,itime) = doubles_ab(:,:)
+       doubles_bb_save(:,:,itime) = doubles_bb(:,:)
 
-       write(200,"(f10.7)",advance='no') time(itime), norm, ( singles(i), i=1, nob )
+    end do
+    !$OMP END DO
+    !$OMP END PARALLEL
+    
+       
+    do itime=1, ntimes
+       !: write singlesa and doubles
+       write(200,"(f10.7)",advance='no') time(itime), norm, ( singles_save(i,itime), i=1, nob )
        do x=1, nob
-          write(200,"(f10.7)",advance='no' ) ( doubles_ab(i,x), i=1, noa )
+          write(200,"(f10.7)",advance='no' ) ( doubles_ab_save(i,x,itime), i=1, noa )
        end do
        do x=1, nob
-          write(200,"(f10.7)",advance='no' ) ( doubles_bb(i,x), i=x+1, nob )
+          write(200,"(f10.7)",advance='no' ) ( doubles_bb_save(i,x,itime), i=x+1, nob )
        end do
        write(200,'(A)') ''
-       
+       !: write ci10_save
+       write(110,"(f10.7)",advance='no') time(itime), norm, ( ci10_save(i,itime), i=1, nob )
+       write(110,'(A)') ''
     end do
-    
-41  format( '   singles ', 6x,    10(1x,f8.5) )
-42  format( ' doublesAB ', i5'B', 10(1x,f8.5) )
-43  format( ' doublesBB ', i5'B', 10(1x,f8.5) ) 
 
-    close(100)
     close(200)
+    close(110)
+
+    write(iout,'(A)') ' --> generated file '//trim(ofile)
+    write(iout,'(A)') ' --> generated file '//trim(ofile10)
+    flush(iout)    
     
     
   end subroutine summarize_coefficients_ip
   !:--------------------------:!
   !:--------------------------:!
-  subroutine write_ci_mod(idir)
-
-
-    use utils
-    implicit none
-
-    integer(8), intent(in) :: idir
-
-    integer(8) :: itime, i 
-    real(8) :: norm2
-    character(5) :: c0
-    character(1000) :: ofile
-
-    
-    write(c0,'(i0)') idir
-    ofile = 'CI_mod'//trim(e_d)//trim(c0)//'.out'
-
-    open( unit=100,file=trim(ofile) )
-
-    do itime=1, ntimes
-       
-       norm2 = norm_sq(itime)
-
-       write(100,'(f13.5)',advance='no') time(itime)
-       write(100,'(f9.5)',advance='no') ( psi(i,itime)*dconjg(psi(i,itime))/norm2, i=1, nstuse )
-       write(100,'(A)')
-
-    end do
-       
-
-
-  end subroutine write_ci_mod
-  !:--------------------------:!
-  !:--------------------------:!  
-  subroutine svd_coefficients(idir) 
-
-    implicit none
-
-    integer(8), intent(in) :: idir
-
-
-  end subroutine svd_coefficients
-  !:--------------------------:!
-  !:--------------------------:!  
-  subroutine write_coefficients(idir)
-
-    implicit none
-    
-    integer(8), intent(in) :: idir
-
-    integer(8) :: i, itime
-
-    character(5) :: c0
-
-    write( c0, '(i0)' ) idir
-    open( unit=144,file='CI_scfyz'//trim(e_d)//trim(c0)//'.out' )
-    
-    do itime=1, ntimes
-       do i=1, nstuse
-          write(144,'(f15.10,1x,f15.10)') real(psi(i,itime)), aimag(psi(i,itime))
-       end do
-    end do
-    
-    close(144)
-
-
-  end subroutine write_coefficients
-  !:--------------------------:!
-  !:--------------------------:!  
 end module misc
